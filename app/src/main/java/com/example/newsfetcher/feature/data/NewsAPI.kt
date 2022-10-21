447e7f102b4b2342c2ed9192abb46cc01487464d
KashbullinRail <kashbullinrail@gmail.com>
2022-10-21 13:40:29 +0300

    формирование post-запроса

--- app/src/main/java/com/example/newsfetcher/feature/data/NewsAPI.kt
+++ app/src/main/java/com/example/newsfetcher/feature/data/NewsAPI.kt
@@ -3,13 +3,18 @@ package com.example.newsfetcher.feature.data
 import com.example.newsfetcher.di.API_KEY
 import com.example.newsfetcher.feature.data.model.ArticleRemoteModel
 import com.example.newsfetcher.feature.data.model.ArticlesRemoteModel
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface NewsAPI {
     @GET("v2/top-headlines")
     suspend fun getArticles(
 //        @Query("apiKey") apiKey: String = API_KEY,
-        @Query("country") units :String = "ru"
+        @Query("country") country :String = "ru"
+    ): ArticlesRemoteModel
+
+    @FormUrlEncoded
+    @POST("v2/top-headlines")
+    suspend fun postArticles(
+        @Field("country") country :String = "ru"
     ): ArticlesRemoteModel
 }
