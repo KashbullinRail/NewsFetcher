61a6f21a2738e27e0a1f8c0b1e843ee38650551d
KashbullinRail <kashbullinrail@gmail.com>
2022-10-22 10:01:30 +0300

    подготовка к доработке вкладки "Избранное"

--- app/src/main/java/com/example/newsfetcher/feature/data/NewsAPI.kt
+++ app/src/main/java/com/example/newsfetcher/feature/data/NewsAPI.kt
@@ -12,23 +12,23 @@ interface NewsAPI {
         @Query("country") country :String = "ru"
     ): ArticlesRemoteModel
 
-    @POST("v2/top-headlines")
-    suspend fun post2Articles(
-        @Body jsonModel: JsonModel.JsonModelItem = JsonModel.JsonModelItem(
-            "f767f1e7-63e2-4f7b-984d-1f4743e7dfd1",
-            "09.12.2013T14:53:16.548Z",
-            "fields_tyson@manglo.degree",
-            "6212577f0095c22f40b1a78a",
-            profile = JsonModel.JsonModelItem.Profile(
-                "Quis labore commodo culpa aliquip cillum deserunt culpa non pariatur minim ullamco reprehenderit nulla esse. Ullamco aliquip do commodo cillum.",
-                "59 Menahan Street, Loretto, Washington",
-                "Manglo",
-                "17.02.1989",
-                location = JsonModel.JsonModelItem.Profile.Location(56.740646, -60.715809),
-                "Fields Tyson"
-            ),
-            roles = listOf("owner", "guest"),
-            "10.12.2013T14:53:16.548Z",
-            "fields89")
-    ): ArticlesRemoteModel
+//    @POST("v2/top-headlines")
+//    suspend fun post2Articles(
+//        @Body jsonModel: JsonModel.JsonModelItem = JsonModel.JsonModelItem(
+//            "f767f1e7-63e2-4f7b-984d-1f4743e7dfd1",
+//            "09.12.2013T14:53:16.548Z",
+//            "fields_tyson@manglo.degree",
+//            "6212577f0095c22f40b1a78a",
+//            profile = JsonModel.JsonModelItem.Profile(
+//                "Quis labore commodo culpa aliquip cillum deserunt culpa non pariatur minim ullamco reprehenderit nulla esse. Ullamco aliquip do commodo cillum.",
+//                "59 Menahan Street, Loretto, Washington",
+//                "Manglo",
+//                "17.02.1989",
+//                location = JsonModel.JsonModelItem.Profile.Location(56.740646, -60.715809),
+//                "Fields Tyson"
+//            ),
+//            roles = listOf("owner", "guest"),
+//            "10.12.2013T14:53:16.548Z",
+//            "fields89")
+//    ): ArticlesRemoteModel
 }
--- app/src/main/java/com/example/newsfetcher/feature/domian/ArticlesInteractor.kt
+++ app/src/main/java/com/example/newsfetcher/feature/domian/ArticlesInteractor.kt
@@ -12,45 +12,45 @@ class ArticlesInteractor(private val repository: ArticlesRepository) {
     suspend fun getArticles() = attempt {
 
 
-        val gson = Gson()
-        val request = gson.toJson(JsonObject().apply {
-            addProperty("count", "5")
-            addProperty("country", "ru")
-            addProperty("list", gson.toJson(listOf("1", "2", "3")))
-        })
-        Log.e("TAG1", request)
-
-        val response = "{\"count\":5,\"country\":\"ru\",\"list\":[\"35573\"]}"
-        val model = gson.fromJson(response, SampleModel::class.java)
-        Log.e("TAG", model.toString())
-
-        val responseJson = "[\n" +
-                "{\n" +
-                "\"id\": \"6212577f0095c22f40b1a78a\",\n" +
-                "\"email\": \"fields_tyson@manglo.degree\",\n" +
-                "\"roles\": [\n" +
-                "\"owner\",\n" +
-                "\"guest\"\n" +
-                "],\n" +
-                "\"apiKey\": \"f767f1e7-63e2-4f7b-984d-1f4743e7dfd1\",\n" +
-                "\"profile\": {\n" +
-                "\"dob\": \"17.02.1989\",\n" +
-                "\"name\": \"Fields Tyson\",\n" +
-                "\"about\": \"Quis labore commodo culpa aliquip cillum deserunt culpa non pariatur minim ullamco reprehenderit nulla esse. Ullamco aliquip do commodo cillum.\",\n" +
-                "\"address\": \"59 Menahan Street, Loretto, Washington\",\n" +
-                "\"company\": \"Manglo\",\n" +
-                "\"location\": {\n" +
-                "\"lat\": 56.740646,\n" +
-                "\"long\": -60.715809\n" +
-                "}\n" +
-                "},\n" +
-                "\"username\": \"fields89\",\n" +
-                "\"createdAt\": \"09.12.2013T14:53:16.548Z\",\n" +
-                "\"updatedAt\": \"10.12.2013T14:53:16.548Z\"\n" +
-                "}\n" +
-                "]"
-        val jsonModel = gson.fromJson(responseJson, JsonModel::class.java)
-        Log.e("TAG", jsonModel.toString())
+//        val gson = Gson()
+//        val request = gson.toJson(JsonObject().apply {
+//            addProperty("count", "5")
+//            addProperty("country", "ru")
+//            addProperty("list", gson.toJson(listOf("1", "2", "3")))
+//        })
+//        Log.e("TAG1", request)
+//
+//        val response = "{\"count\":5,\"country\":\"ru\",\"list\":[\"35573\"]}"
+//        val model = gson.fromJson(response, SampleModel::class.java)
+//        Log.e("TAG", model.toString())
+//
+//        val responseJson = "[\n" +
+//                "{\n" +
+//                "\"id\": \"6212577f0095c22f40b1a78a\",\n" +
+//                "\"email\": \"fields_tyson@manglo.degree\",\n" +
+//                "\"roles\": [\n" +
+//                "\"owner\",\n" +
+//                "\"guest\"\n" +
+//                "],\n" +
+//                "\"apiKey\": \"f767f1e7-63e2-4f7b-984d-1f4743e7dfd1\",\n" +
+//                "\"profile\": {\n" +
+//                "\"dob\": \"17.02.1989\",\n" +
+//                "\"name\": \"Fields Tyson\",\n" +
+//                "\"about\": \"Quis labore commodo culpa aliquip cillum deserunt culpa non pariatur minim ullamco reprehenderit nulla esse. Ullamco aliquip do commodo cillum.\",\n" +
+//                "\"address\": \"59 Menahan Street, Loretto, Washington\",\n" +
+//                "\"company\": \"Manglo\",\n" +
+//                "\"location\": {\n" +
+//                "\"lat\": 56.740646,\n" +
+//                "\"long\": -60.715809\n" +
+//                "}\n" +
+//                "},\n" +
+//                "\"username\": \"fields89\",\n" +
+//                "\"createdAt\": \"09.12.2013T14:53:16.548Z\",\n" +
+//                "\"updatedAt\": \"10.12.2013T14:53:16.548Z\"\n" +
+//                "}\n" +
+//                "]"
+//        val jsonModel = gson.fromJson(responseJson, JsonModel::class.java)
+//        Log.e("TAG", jsonModel.toString())
 
 
         repository.getArticles()
