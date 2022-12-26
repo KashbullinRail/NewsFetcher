0f79744e52e067357e8a094f67dc33449dbd11d6
KashbullinRail <kashbullinrail@gmail.com>
2022-12-26 22:37:18 +0300

    add content for NewsAPI.kt

--- app/src/main/java/com/example/newsfetcher/feature/main_screen/data/NewsAPI.kt
+++ app/src/main/java/com/example/newsfetcher/feature/main_screen/data/NewsAPI.kt
@@ -1,15 +1,43 @@
 package com.example.newsfetcher.feature.main_screen.data
 
 import com.example.newsfetcher.feature.main_screen.data.model.ArticlesRemoteModel
+import org.intellij.lang.annotations.Language
 import retrofit2.http.*
+import java.security.ProtectionDomain
 
 
 interface NewsAPI {
 
-    @GET("v2/top-headlines")
+//    @GET("v2/top-headlines")
+//    suspend fun getArticles(
+//        @Query("country") country: String = "ru",
+//        @Query("language") language: String = "ru",
+//        @Query("category") category: String = "general",
+//        @Query("sources") sources: String = "",
+//        @Query("q") query: String = "канделаки",
+//        @Query("pageSize") pageSize: String = "",
+//        @Query("page") page: String = ""
+//    ): ArticlesRemoteModel
+
+    @GET("v2/everything")
     suspend fun getArticles(
-        @Query("country") country: String = "ru"
+        @Query("q") query: String = "москвич",
+//        @Query("sources") sources: String = "",
+//        @Query("domains") domain: String = "",
+//        @Query("from") from: String = "",
+//        @Query("to") to: String = "",
+//        @Query("language") language: String = "ru",
+//        @Query("sortBy") sortBy: String = "",
+//        @Query("pageSize") pageSize: String = "",
+//        @Query("page") page: String = ""
     ): ArticlesRemoteModel
 
+
+//    @GET("v2/source")
+//    suspend fun getArticles(
+//        @Query("category") category: String = "канделаки",
+//        @Query("language") language: String = "ru",
+//        @Query("country") country: String = "ru"
+//    ): ArticlesRemoteModel
 
 }
