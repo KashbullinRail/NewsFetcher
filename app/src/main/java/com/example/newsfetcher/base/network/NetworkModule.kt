b3f714925e6ee65abf70cac6dde8b36abdb3145e
KashbullinRail <kashbullinrail@gmail.com>
2023-03-19 22:08:39 +0300

    corrected NetworkModule.kt

--- app/src/main/java/com/example/newsfetcher/base/network/NetworkModule.kt
+++ app/src/main/java/com/example/newsfetcher/base/network/NetworkModule.kt
@@ -10,6 +10,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+
 val networkModule = module {
 
     single<HttpLoggingInterceptor> {
